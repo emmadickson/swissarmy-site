92dfc1861deeabef03d4c8508cb20f88f6a6719f
Emma Dickson <emmadickson@Emmas-MacBook-Pro-2.local>
2020-07-17 12:42:11 -0400

    add requirements.txt

--- extract_page.py
+++ extract_page.py
@@ -25,19 +25,23 @@ dict['scripts'] = []
 
 for link in soup.find_all('a', href=True):
     found_link = (link['href'])
-    dict['a_tag'].append(found_link)
+    if len(found_link) > 2 and found_link not in dict['a_tag']:
+        dict['a_tag'].append(found_link)
     
 for link in soup.find_all('img'):
     img = (link['src'])  
-    dict['img'].append(img)
+    if len(img) > 2 and img not in dict['img']:
+        dict['img'].append(img)
 
 for link in soup.find_all('script'):
     script = (link['src'])    
-    dict['scripts'].append(script)
+    if len(script) > 2 and script not in dict['scripts']:
+        dict['scripts'].append(script)
     
 for link in soup.find_all('link'):
     link = (link['href'])   
-    dict['link_tag'].append(found_link) 
+    if len(link) > 2 and link not in dict['link_tag']:
+        dict['link_tag'].append(link) 
     
 r = json.dumps(dict)
 with open(filename, "w") as f:
